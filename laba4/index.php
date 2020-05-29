c8e8c37151781fdcb39728093c0b77502eca3bd1
Dudkin-Nikita <64280767+Dudkin-Nikita@users.noreply.github.com>
2020-05-29 12:32:13 +0300

    Add files via upload

---
+++ laba4/index.php
@@ -0,0 +1,15 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-32">
+</head>
+<body>
+<h1>Вариант 6</h1>
+<form method="post" action="result.php" enctype="multipart/form-data">
+    <label for="inputfile">Выбрать файл</label>
+    <input type="file" id="inputfile" name="inputfile"></br>
+    <input type="submit" value="Загрузить">
+</form>
+</form>
+</body>
+</html>
