f005ea5e520c33a6983cf2a8bad6854783f80572
Dudkin-Nikita <64280767+Dudkin-Nikita@users.noreply.github.com>
2020-05-29 12:30:45 +0300

    Add files via upload

---
+++ laba2/services.php
@@ -0,0 +1,9 @@
+<DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-32">
+</head>
+<body>
+	<h1>Services</h1>
+</body>
+</html>
---
+++ laba2/var6.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-32">
+</head>
+<body>
+<h1>Вариант 6</h1>
+<h3>Введите пятимерный массив (Значения разделяются запятыми)</h3>
+<form action="result.php" method="POST">
+    Первый уровень: <input type="text" name="1"/><br><br>
+    Второй уровень: <input type="text" name="2"/><br><br>
+    Третий уровень: <input type="text" name="3"/><br><br>
+    Четвертый уровень: <input type="text" name="4"/><br><br>
+    Пятый уровень: <input type="text" name="5"/><br><br>
+    <input type="submit" value="Ввод">
+</form>
+</body>
+</html>
